e6b67f9bde84e11708cbb8b10894aad35ffbf9b3
Gaoprea <Gabriel.oprea@endava.com>
2020-01-14 10:05:09 +0200

    First course

--- Zoo/src/Zoo/Zoo.java
+++ Zoo/src/Zoo/Zoo.java
@@ -1,4 +1,22 @@
 package Zoo;
 
 public class Zoo {
+
+    void conserveAnimal(String animal, int nrConserve) {
+        switch (nrConserve) {
+            case 1: System.out.println(animal + " mananca o conserva zilnic"); break;
+            case 0: System.out.println(animal + " nu mananca conserve"); break;
+            default: System.out.println(animal + " mananca " + nrConserve + " conserve pe zi");
+        }
+    }
+
+    public static void main(String[] args) {
+    Zoo zoo = new Zoo();
+
+    Animal pisica = new Animal("pisica",3);
+    zoo.conserveAnimal(pisica.getNume(), pisica.getNrConserve());
+
+    Animal catel = new Animal("catel",1);
+    zoo.conserveAnimal(catel.getNume(), catel.getNrConserve());
+    }
 }
