40d415e4726444d23b9e8e3b7e7039b4f1eb6f0a
dlivezeanu <Diana.Livezeanu@justbe.co.uk>
2020-01-21 09:06:11 +0200

    three stooges

--- Zoo/src/Zoo/Animal.java
+++ Zoo/src/Zoo/Animal.java
@@ -1,4 +1,28 @@
 package Zoo;
 
 public class Animal {
+    private String nume;
+    private int nrConserve;
+
+    public Animal(final String nume, final int nrConserve) {
+        this.nume = nume;
+        this.nrConserve = nrConserve;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(final String nume) {
+        this.nume = nume;
+    }
+
+    public int getNrConserve() {
+        return nrConserve;
+    }
+
+    public void setNrConserve(final int nrConserve) {
+        this.nrConserve = nrConserve;
+    }
+
 }
--- Zoo/src/Zoo/Zoo.java
+++ Zoo/src/Zoo/Zoo.java
@@ -1,4 +1,44 @@
 package Zoo;
 
 public class Zoo {
+
+    void conserveAnimal(String animal, int nrConserve) {
+        switch(nrConserve) {
+            case 1: System.out.println(animal + " mananca o conserva pe zi"); break;
+            case 0: System.out.println(animal + " nu mananca deloc conserve"); break;
+            default: System.out.println(animal + " mananca " + nrConserve + " conserve pe zi");
+        }
+        //
+        // if (nrConserve > 1) {
+        //     System.out.println(animal + " mananca " + nrConserve + " conserve pe zi");
+        // } else if (nrConserve > 0) {
+        //     System.out.println(animal + " mananca o conserva pe zi");
+        // } else {
+        //     System.out.println(animal + " nu mananca deloc conserve");
+        // }
+    }
+
+    static void conserveAnimal(Animal animal) {
+        switch(animal.getNrConserve()) {
+            case 1: System.out.println(animal.getNume() + " mananca o conserva pe zi"); break;
+            case 0: System.out.println(animal.getNume() + " nu mananca deloc conserve"); break;
+            default: System.out.println(animal.getNume() + " mananca " + animal.getNrConserve() + " conserve pe zi");
+        }
+    }
+
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+
+        Animal pisica = new Animal("pisica", 2);
+        conserveAnimal(pisica);
+
+        String catel = "catelul";
+        int nrConserveCatel = 1;
+        zoo.conserveAnimal(catel, nrConserveCatel);
+
+        String cal = "calul";
+        int nrConserveCal = 0;
+        zoo.conserveAnimal(cal, nrConserveCal);
+
+    }
 }
